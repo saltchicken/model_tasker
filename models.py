75c791cedd7c271e788122b75749144a73e04413
saltchicken <john.eicher89@gmail.com>
2023-05-30 22:17:16 -0700

    Ugly fix. Interuption of Python REPL glitches program.

--- models.py
+++ models.py
@@ -55,7 +55,7 @@ class CommandModel():
             'switch': switch_desktop,
         }
         
-    def run(self, transcription):
+    def run(self, transcription, **kwargs):
         command = remove_punctuation_except_apostrophe(transcription)
         command = command.lower().split(" ")
         logger.debug(f"CommandModel received command: {command}")
@@ -67,6 +67,8 @@ class CommandModel():
                 logger.debug(f"CommandModel Running key: {key} param1: {param1}")
                 execute = self.command_mapping[key]
                 execute(param1)
+            else:
+                logger.debug("command not found")
         else:
             logger.debug("command not found")
         
@@ -80,7 +82,7 @@ class DefinitionModel():
         Definition: This is the definition:"""
         self.prompt_template = PromptTemplate(input_variables=['word'], template=self.template)
         self.definition_chain = LLMChain(llm=self.llm, prompt=self.prompt_template)
-    def run(self, word):
+    def run(self, word, **kwargs):
         self.screen.write(word)
         # result = self.definition_chain({"word": word})
         # say_elvenlabs(result['text'])
@@ -97,29 +99,35 @@ class DefinitionModel():
 class TypewriteModel():
     def __init__(self):
         pass
-    def run(self, word):
+    def run(self, word, **kwargs):
         pyautogui.typewrite(word)
         # pyautogui.press('enter')
         
 class EchoModel():
     def __init__(self, screen):
         self.screen = screen
-    def run(self, word):
+    def run(self, word, **kwargs):
         self.screen.write(word)
         
 class MyCustomHandler(BaseCallbackHandler):
-    def __init__(self, app) -> None:
+    def __init__(self, wait, mutex, show_dialog) -> None:
         super().__init__()
-        # TODO Make this the window and rename it for input interrupt.
-        self.app = app
-        print(self.app.worker)
+        self.wait = wait
+        self.mutex = mutex
+        self.show_dialog = show_dialog
+        self.cont = None
     # def on_llm_new_token(self, token: str, **kwargs) -> None:
     #     print(f"My custom handler, token: {token}")
     def on_tool_start(self, serialized: Dict[str, Any], input_str: str, **kwargs: Any) -> Any:
         logger.debug("Tool starting...")
-        cont = self.app.window.showDialog()
-        if not cont:
-            # TODO: Should not be closing the whole app. Stop only agentChain
+        self.show_dialog.emit()
+        self.mutex.lock()
+        self.wait.wait(self.mutex)
+        self.mutex.unlock()
+        if self.cont == None:
+            logger.debug("CRITICAL ERROR: No cont provided")
+        if self.cont == False:
+            # TODO: This kills the ModelRunner thread and breaks the app. Doesn't finish execution which is critical to continue main thread.
             sys.exit(1)
         logger.debug("Tool continuing...")
     def on_agent_action(self, action: AgentAction, **kwargs: Any) -> Any:
@@ -134,8 +142,15 @@ class PythonREPLModel():
         # llm=OpenAI(temperature=0, max_tokens=1000, streaming=True, callbacks=[MyCustomHandler()]),
         llm=OpenAI(temperature=0, max_tokens=1000),
         tool=PythonREPLTool(),
-        verbose=True
+        verbose=True,
+        # return_intermediate_steps = True
     )
-    def run(self, goal):
+        
+    def run(self, goal, **kwargs):
         # agent_executor.run(goal, callbacks=[MyCustomHandler()])
-        self.agent_executor.run("""Understand, write a python script that will print "hello world""", callbacks=[MyCustomHandler(self.app)])
+        
+        self.custom_handler = MyCustomHandler(kwargs['wait'], kwargs['mutex'], kwargs['show_dialog'])
+        if 'wait' in kwargs and 'mutex' in kwargs and 'show_dialog' in kwargs:
+            self.agent_executor.run("""Understand, write a python script that will print "hello world""", callbacks=[self.custom_handler])
+        else:
+            logger.debug("No wait or mutex or show_dialog or cont provided")
